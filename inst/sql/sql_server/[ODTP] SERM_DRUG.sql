46f4497c8ac471d3b7c2657daa8cb146e7b607e7
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2024-04-04 14:57:00 +0900

    Update [ODTP] SERM_DRUG.sql
    Update sql for ORACLE database

--- inst/sql/sql_server/[ODTP] SERM_DRUG.sql
+++ inst/sql/sql_server/[ODTP] SERM_DRUG.sql
@@ -3,8 +3,8 @@ DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, drug_concept_id, cohort_start_date, cohort_end_date, days_supply, duration)
 select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, d.duration
 from (
-  select de.*, 1 as 'duration'
+  SELECT de.person_id, de.drug_concept_id, de.drug_exposure_start_date, de.drug_exposure_end_date, de.days_supply, 1 AS duration
   FROM @cdm_database_schema.DRUG_EXPOSURE de
-  WHERE drug_concept_id in (41181419,21602626,44814412,21602625)
-	and days_supply >= 28
+  WHERE de.drug_concept_id IN (41181419, 21602626, 44814412, 21602625)
+    AND de.days_supply >= 28
   ) d;
