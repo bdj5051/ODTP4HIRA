ad37aa6045f6e64f972ccb4a32670a5a0248a91a
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2024-04-04 14:49:41 +0900

    Update [ODTP] Bisphosphonate PO_DRUG.sql
    Update sql for ORACLE database

--- inst/sql/sql_server/[ODTP] Bisphosphonate PO_DRUG.sql
+++ inst/sql/sql_server/[ODTP] Bisphosphonate PO_DRUG.sql
@@ -3,13 +3,13 @@ DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, drug_concept_id, cohort_start_date, cohort_end_date, days_supply, duration)
 select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, d.duration
 from (
-  select de.*, 7 as 'duration'
+  SELECT de.person_id, de.drug_concept_id, de.drug_exposure_start_date, de.drug_exposure_end_date, de.days_supply, 7 AS duration
   FROM @cdm_database_schema.DRUG_EXPOSURE de
-  WHERE drug_concept_id in (40173612,21604152,42943685,40174494,21604155)
-	and days_supply >= 4
+  WHERE de.drug_concept_id IN (40173612, 21604152, 42943685, 40174494, 21604155) 
+	AND de.days_supply >= 4
 union all
-  select de.*, 30 as 'duration'
+  SELECT de.person_id, de.drug_concept_id, de.drug_exposure_start_date, de.drug_exposure_end_date, de.days_supply, 30 AS duration
   FROM @cdm_database_schema.DRUG_EXPOSURE de
-  WHERE drug_concept_id in (40174364,40174486)
-	and days_supply >= 1
+  WHERE de.drug_concept_id IN (40174364, 40174486)
+    AND de.days_supply >= 1
   ) d;
